56fc1cfea902c8eb5ef7a75c6eb80507387d6f69
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2013-01-24 21:47:39 -0500

    make the terminal emulator used specifiable

--- test/start.sh
+++ test/start.sh
@@ -3,6 +3,20 @@
 display=:3
 xauthFile=/tmp/xephyr.auth
 xephyrCmd=$(which Xephyr 2>/dev/null)
+termEmu="gnome-terminal"
+
+while getopts ":t:" opt
+do
+    case $opt in
+        t) termEmu=$OPTARG;;
+    esac
+done
+
+termEmuCmd=$(which $termEmu 2>/dev/null)
+[ "${termEmuCmd:-undef}" == "undef" ] && {
+    echo "can't find $termEmu, specify a terminal emulator with -t"
+    exit 1;
+}
 
 [ "${xephyrCmd:-undef}" == "undef" ] && {
     echo "you need to install Xephyr to run this script!"
@@ -15,7 +29,7 @@ export XAUTHORITY=$xauthFile
 export DISPLAY=$display
 sleep 3
 
-gnome-terminal &
+$terEmuCmd &
 
 #xsetroot  -solid gray -cursor_name left_ptr
 
